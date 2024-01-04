1eb3d18ed9dd494de2075a88b47a830ac5afb26a
spirithuntt <nouhaila.elhousnii@gmail.com>
2024-01-04 14:08:46 +0100

    made the environments file + prepared the auth.service

--- TaskFlow/src/app/services/auth/auth.service.ts
+++ TaskFlow/src/app/services/auth/auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { AuthenticationResponseDTO } from 'src/app/dto/auth/responses/auth-response.dto';
+import { RegisterRequestDTO } from 'src/app/dto/auth/requests/register-request.dto';
+import { AuthenticationRequestDTO } from 'src/app/dto/auth/requests/auth-request.dto';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+    ) {}
+
+  register(registerRequest: RegisterRequestDTO ): Observable< AuthenticationResponseDTO > {
+    return this.http.post<any>(`${environment.apiUrl}/register`, registerRequest);
+  }
+
+  authenticate(authenticationRequest: AuthenticationRequestDTO ): Observable< AuthenticationResponseDTO > {
+    return this.http.post<any>(`${environment.apiUrl}/authenticate`, authenticationRequest);
+  }
+
 }
